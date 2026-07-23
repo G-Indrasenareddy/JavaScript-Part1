08915994cb34f7f34b060a94d40d127129e2e232
Indrasena Reddy <indrasenareddygummireddy2005@gmail.com>
2026-07-23 18:28:19 +0530

    Initial the javaScript

---
+++ crud.js
@@ -0,0 +1,13 @@
+//create variable
+var eid = 101
+
+//Read the variable
+console.log(eid)
+
+//update the variable
+eid = 202
+console.log(eid)
+
+//Delete the variable
+eid = undefined //or we can write the null instead of undefined
+console.log(eid)
---
+++ one.js
@@ -0,0 +1,9 @@
+var ename = "Rahul Gandhi"
+
+function getDetails(){
+    console.log("Displaying the Employee details")
+}
+class employee{
+
+}
+var order = {}
